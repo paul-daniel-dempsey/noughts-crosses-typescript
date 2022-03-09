690806421c4506faf52849e3ea8811a2c093ea1a
Paul.Dempsey <paul.daniel.dempsey@icloud.com>
2022-03-09 13:28:38 +0000

    Interface TS Board component added

--- src/noughtCross.test.ts
+++ src/noughtCross.test.ts
@@ -1,67 +1,68 @@
 import { findWinner } from './noughtCross';
+import { IBoard } from './board';
 
 describe("findWinner", () => {
 
-    let board_downwardX =  [
+    let board_downwardX = {places : [
             ["X", "0", null],
             ["X", null, "0"],
             ["X", null, "0"]
-            ];
+            ]};
 
-    let board_downward0 =  [
+    let board_downward0 = {places : [
             ["X", "0", "0"],
             ["X", null, "0"],
             [null, "X", "0"]
-            ];
-    
-    let board_across0 =  [
+            ]};
+
+    let board_across0 = {places : [
             ["X", null, "X"],
             ["0", null, "X"],
             ["0", "0", "0"]
-            ];
+            ]}
 
-    let board_acrossX_LC =  [
-            ["x", "x", "x"],
+    let board_acrossX_LC = {places : [
+        ["x", "x", "x"],
+        ["0", null, "0"],
+        ["x", null, "0"]
+        ]};
+
+    let board_diagnolX_LC = {places : [
+        ["X", null, "X"],
+        ["0", "x", "0"],
+        ["0", null, "X"]
+        ]};
+
+    let board_diagnol0 = {places : [
             ["0", null, "0"],
-            ["x", null, "0"]
-            ];
-
-    let board_diagnolX_LC =  [
-            ["X", null, "X"],
-            ["0", "x", "0"],
+            ["X", "0", "X"],
             ["0", null, "X"]
-            ];
+            ]};
 
-    let board_diagnol0 =  [
-                ["0", null, "0"],
-                ["X", "0", "X"],
-                ["0", null, "X"]
-                ];
+    let board_none = {places : [
+        ["0", "X", "0"],
+        ["X", "0", "0"],
+        ["X", "0", "X"]
+        ]};
 
-    let board_none =  [
-            ["0", "X", "0"],
-            ["X", "0", "0"],
-            ["X", "0", "X"]
-            ];
-
-    let board_empty =  [
+    let board_empty = {places : [
             [null, null, null],
             [null, null, null],
             [null, null, null]
-            ];
+            ]};
 
-    test("Find winner, vertical 3xX or 3x0", () => {         
-        expect(findWinner(board_downwardX)).toEqual("X"); 
-        });
-    test("Find winner, vertical 3xX or 3x0", () => {         
-        expect(findWinner(board_downward0)).toEqual("0"); 
-        });
-    test("Find (lowercase) winner, horizontal 3xX or 3x0", () => {         
+    test("Find (lowercase) winner, horizontal 3xX", () => {         
         expect(findWinner(board_acrossX_LC)).toEqual("X"); 
     });
-    test("Find winner, horizontal 3xX or 3x0", () => {         
+    test("Find winner, horizontal 3x0", () => {         
         expect(findWinner(board_across0)).toEqual("0"); 
     });
+    test("Find winner, vertical 3xX", () => {         
+        expect(findWinner(board_downwardX)).toEqual("X"); 
+        });
+    test("Find winner, vertical 3x0", () => {         
+        expect(findWinner(board_downward0)).toEqual("0"); 
+        });
     test("Find (lowercase) winner, diagonal 3xX or 3x0", () => {         
         expect(findWinner(board_diagnolX_LC)).toEqual("X"); 
     });
--- src/noughtCross.ts
+++ src/noughtCross.ts
@@ -8,42 +8,47 @@
  * The function should return "X" if player X has won, "0" if the player 0 has won, and null if there is currently no winner.
  * @param {Array} board
  */
- export function findWinner ( board : string [][] ) {
-  if (board === undefined) throw new Error("board is required");
+ import { IBoard } from './board';
 
-  // Find horizontal, vertical (90degree rotate) and diagonal winners, drop out early as possible
-  let winner = findRowWinner(board); 
-  if (winner === null) {
-    winner = findRowWinner(board[0].map((_, index) => board.map(row => row[index]).reverse()));// Vertical Winner (ROTATE!)
-    if  (winner === null) {
-      winner = findDiagonalWinner(board);
-    }
-  }
-  return winner;
-};
-
-const findRowWinner = (board : string[][])  => {
-  let found = null;
-  board.forEach(row => { 
-    const across = row.join("").toUpperCase();
-    if (across === "XXX" || across === "000" ) {
-      found = across.substring(0,1).toString();}
-  });
-  return found;
-};
-
-const findDiagonalWinner = (board : string[][])  => {
-    let found = null;
-    for(let i=0;i<=2; i += 2) {
-      if (board[0][i] !== null && board[1][1] !== null && board[2][i] !== null) {
-        let diag = (board[0][i]+board[1][1]+board[2][i]).toString().toUpperCase();
-      if (diag === "XXX" || diag === "000") {
-        found =  diag.substring(0,1).toString(); }
-      }
-    }
-    return found;
-};
-
-module.exports = {
-  findWinner
-};
+ export function findWinner( board : IBoard )
+  {
+     if (board === undefined) throw new Error("board is required");
+   
+     // Find horizontal, vertical (90degree rotate) and diagonal winners, drop out early as possible
+     let winner = findRowWinner(board); 
+     if (winner === null) {
+       let boardVertical = {places: board.places[0].map((_, index) => board.places.map(row => row[index]).reverse())};
+       winner = findRowWinner(boardVertical);// Vertical Winner (ROTATE!)
+       if  (winner === null) {
+         winner = findDiagonalWinner(board);
+       }
+     }
+     return winner;
+   };
+   
+   const findRowWinner = (board : IBoard) => {
+     let found = null;
+     board.places.forEach(row => { 
+       const across = row.join("").toUpperCase();
+       if (across === "XXX" || across === "000" ) {
+         found = across.substring(0,1).toString();}
+     });
+     return found;
+   };
+   
+   const findDiagonalWinner = (board : IBoard) => {
+       let found = null;
+       for(let i=0;i<=2; i += 2) {
+         if (board.places[0][i] !== null && board.places[1][1] !== null && board.places[2][i] !== null) {
+           let diag = (board.places[0][i]+board.places[1][1]+board.places[2][i]).toString().toUpperCase();
+         if (diag === "XXX" || diag === "000") {
+           found =  diag.substring(0,1).toString(); }
+         }
+       }
+       return found;
+   };
+ 
+   module.exports = {
+     findWinner
+   };
+ 
