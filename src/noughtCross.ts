477185f3ca33f8ca760d3e9a04ef7b1fbb104df2
Paul.Dempsey <paul.daniel.dempsey@icloud.com>
2022-03-09 13:25:58 +0000

    Replaced TS 'any' with defined type ( string[][])

--- src/noughtCross.ts
+++ src/noughtCross.ts
@@ -8,7 +8,7 @@
  * The function should return "X" if player X has won, "0" if the player 0 has won, and null if there is currently no winner.
  * @param {Array} board
  */
- export function findWinner ( board ) {
+ export function findWinner ( board : string [][] ) {
   if (board === undefined) throw new Error("board is required");
 
   // Find horizontal, vertical (90degree rotate) and diagonal winners, drop out early as possible
@@ -22,23 +22,23 @@
   return winner;
 };
 
-const findRowWinner = board => {
+const findRowWinner = (board : string[][])  => {
   let found = null;
   board.forEach(row => { 
     const across = row.join("").toUpperCase();
     if (across === "XXX" || across === "000" ) {
-      found = across.substring(0,1).toString("");}
+      found = across.substring(0,1).toString();}
   });
   return found;
 };
 
-const findDiagonalWinner = board => {
+const findDiagonalWinner = (board : string[][])  => {
     let found = null;
     for(let i=0;i<=2; i += 2) {
       if (board[0][i] !== null && board[1][1] !== null && board[2][i] !== null) {
-        let diag = (board[0][i]+board[1][1]+board[2][i]).toString("").toUpperCase();
+        let diag = (board[0][i]+board[1][1]+board[2][i]).toString().toUpperCase();
       if (diag === "XXX" || diag === "000") {
-        found =  diag.substring(0,1).toString(""); }
+        found =  diag.substring(0,1).toString(); }
       }
     }
     return found;
